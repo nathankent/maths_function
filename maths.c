3aabb2e6bc937d17d11549387e3da8cbcde01a06
Nathan <nathan@localhost.localdomain>
2015-02-02 14:56:44 +0000

    Addition function

---
+++ maths.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main()
+{
+int a;
+int b;
+int c;
+
+printf("Please input variable a & b\n");
+scanf(" %i", &a);
+scanf(" %i", &b);
+
+printf("Addition\n");
+c = a + b;
+printf("\n%i + %i = %i\n", a, b, c);
+}
